33fc339a37002b7e8e8f3d6c437695fe7f29ea69
oleg-fit <oleg26-spb@rambler.ru>
2019-09-01 01:02:44 +0300

    Урок 1 и Урок 2

---
+++ js1_lesson_2/app.js
@@ -0,0 +1,15 @@
+let year = 1987;
+let currentYear = 2019;
+let age = currentYear - year;
+let myName = 'Oleg' ;
+let p = document.querySelector('#out');
+let a = 'Hello'; 
+let b = ' '; 
+let c = 'world'; 
+
+
+console.log(age);
+alert(myName);
+p.innerHTML = '<i>' + age + '</i>';
+console.log(a+b+c);
+console.log(c+a+b);
